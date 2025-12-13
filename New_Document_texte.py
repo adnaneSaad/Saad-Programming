d8d1cfd1cc8fb85e0dea70263be184b218531312
adnaneSaad <166955013+adnaneSaad@users.noreply.github.com>
2025-12-13 20:20:15 +0100

    Remove unnecessary stripping of input for questions

--- New_Document_texte.py
+++ New_Document_texte.py
@@ -6,7 +6,7 @@ tech = 0
 last = ""
 
 print("What's the best subject for you: Math, Science, English, Arabic or ICT?")
-Q1 = input("==>").strip().lower()
+Q1 = input("==>")
 if Q1 == "science":
     scientific = scientific + 4
     tech = tech + 1
@@ -21,14 +21,14 @@ elif Q1 == "ict":
     tech = tech + 4
 
 print("Do you like experiments??")
-Q2 = input("==>").strip().lower()
+Q2 = input("==>")
 if Q2 == "yes":
     scientific = scientific + 3
 elif Q2 == "no":
     scientific = scientific
 
 print("Do you enjoy Mathematics?")
-Q3 = input("==>").strip().lower()
+Q3 = input("==>")
 if Q3 == "yes":
     scientific = scientific + 3
     tech = tech + 2
@@ -37,14 +37,14 @@ elif Q3 == "no":
     tech = tech
 
 print("Do you like writing stories, books, or reading them?")
-Q4 = input("==>").strip().lower()
+Q4 = input("==>")
 if Q4 == "yes":
     languistic = languistic + 3   
 elif Q4 == "no":
     languistic = languistic
 
 print("Do you like devices, computers or programming?")
-Q5 = input("==>").strip().lower()
+Q5 = input("==>")
 if Q5 == "yes":
     tech = tech + 4
 elif Q5 == "no":
