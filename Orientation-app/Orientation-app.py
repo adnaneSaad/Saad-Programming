f3f211dadac95591e63a7c87d7ff3104e2f76420
saadadnane <saadadnane2202@gmail.com>
2026-02-20 15:42:25 +0000

    Auto-update 2026-02-20 15:42:25

--- Orientation-app/Orientation-app.py
+++ Orientation-app/Orientation-app.py
@@ -1,88 +1,123 @@
 #!/usr/bin/env python3
 # coding: utf-8
+# Upwards I'm just telling your PC to run this as a Python code "the first line isn't needed in Windows"
+# In the second line I'm adding support to imogies, french accents and arabic letters "in case I needed them"
 import tkinter as tk
-from tkinter import messagebox, ttk
+from tkinter import ttk
+# first line is importing a library that I need and aliasing it as tk
+# And the second I'm importing ttk "Themed tkinter"
 
 root = tk.Tk()
 root.title("Saad's Orientation Assistant")
+# Creating main frame and title
 #==========================================================================================================================================#
+# Downwards I'm creating labels and entries "simply design" and tricking tkinter so I can use ".grid" and ".pack" in the same window
 MainLabel1 = tk.Label(root, text = "Welcome to Saad's Orientation app for 2AC!", font=("Arial", 16, "bold") ,foreground="Blue")
 MainLabel1.pack()
 
 MainLabel2 = tk.Label(root, text = "Write down your marks to see which orientation is best for you", font=("Arial", 12), pady = 10)
 MainLabel2.pack()
-#==========================================================================================================================================#
+
+
 Frame = ttk.Frame(root)
 Frame.pack(pady = 20)
-#==========================================================================================================================================#
+
+
 Label1 = ttk.Label(Frame, text = "Physics:", font = ("Arial", 10))
 Label1.grid(row = 1, column = 0, sticky = "w", padx = 10, pady = 10)
 
 Entry1 = ttk.Entry(Frame)
 Entry1.grid(row = 1, column = 1, sticky = "w", padx = 10, pady = 10)
-#==========================================================================================================================================#
+
+
 Label2 = ttk.Label(Frame, text = "Hist.Geo:", font = ("Arial", 10))
 Label2.grid(row = 2, column = 0, sticky = "w", padx = 10, pady = 10)
 
 Entry2 = ttk.Entry(Frame)
 Entry2.grid(row = 2, column = 1, sticky = "w", padx = 10, pady = 10)
-#==========================================================================================================================================#
+
+
 Label3 = ttk.Label(Frame, text = "ICT:", font = ("Arial", 10))
 Label3.grid(row = 3, column = 0, sticky = "w", padx = 10, pady = 10)
 
 Entry3 = ttk.Entry(Frame)
 Entry3.grid(row = 3, column = 1, sticky = "w", padx = 10, pady = 10)
-#==========================================================================================================================================#
+
+
 Label4 = ttk.Label(Frame, text = "Edu.Islamique", font = ("Arial", 10))
 Label4.grid(row = 4, column = 0, sticky = "w", padx = 10, pady = 10)
 
 Entry4 = ttk.Entry(Frame)
 Entry4.grid(row = 4, column = 1, sticky = "w", padx = 10, pady = 10)
 
-#==========================================================================================================================================#
+
 Label5 = ttk.Label(Frame, text = "English", font = ("Arial", 10))
 Label5.grid(row = 5, column = 0, sticky = "w", padx = 10, pady = 10)
 
 Entry5 = ttk.Entry(Frame)
 Entry5.grid(row = 5, column = 1, sticky = "w", padx = 10, pady = 10)
-#==========================================================================================================================================#
+
+
 Label6 = ttk.Label(Frame, text = "Arabic", font = ("Arial", 10))
 Label6.grid(row = 6, column = 0, sticky = "w", padx = 10, pady = 10)
 
 Entry6 = ttk.Entry(Frame)
 Entry6.grid(row = 6, column = 1, sticky = "w", padx = 10, pady = 10)
-#==========================================================================================================================================#
+
+
 Label7 = ttk.Label(Frame, text = "French", font = ("Arial", 10))
 Label7.grid(row = 7, column = 0, sticky = "w", padx = 10, pady = 10)
 
 Entry7 = ttk.Entry(Frame)
 Entry7.grid(row = 7, column = 1, sticky = "w", padx = 10, pady = 10)
-#==========================================================================================================================================#
+
+
 Label8 = ttk.Label(Frame, text = "Maths", font = ("Arial", 10))
 Label8.grid(row = 8, column = 0, sticky = "w", padx = 10, pady = 10)
 
 Entry8 = ttk.Entry(Frame)
 Entry8.grid(row = 8, column = 1, sticky = "w", padx = 10, pady = 10)
-#==========================================================================================================================================#
+
+
 Label9 = ttk.Label(Frame, text = "SVT", font = ("Arial", 10))
 Label9.grid(row = 9, column = 0, sticky = "w", padx = 10, pady = 10)
 
 Entry9 = ttk.Entry(Frame)
 Entry9.grid(row = 9, column = 1, sticky = "w", padx = 10, pady = 10)
 #==========================================================================================================================================#
+# Saving mechanism; grabs grades and the best orientation and put'em in a file called: Orientation_Result.txt
+def save_to_file(result, marks):
+    import os
+
+    file_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        "Orientation_Result.txt"
+    )
+
+    with open(file_path, "w", encoding="utf-8") as file:
+        file.write("=== Here Are Your Marks And Your Best Orientation! ===\n")
+        for subject, value in marks.items():
+            file.write(f"{subject}: {value}\n")
+        file.write(f"Best Orientation: {result}\n\n")
+#===========================================================================================================================================#
+# Basically this is the core of the program "plus the last part" here, I'm grabbing user inputs, getting sure that the user had filled all entries with supported answers, counting the best Orientation, creating a label with it, and finally calling the saving mechanism whenever the user click that Submit button
+
 def Submit():
     global Physics, HistGeo, ICT, EduIslamique, English, Arabic, French, Maths, SVT
+    try:
+        Physics           = float (Entry1.get())
+        HistGeo           = float (Entry2.get())
+        ICT               = float (Entry3.get())
+        EduIslamique      = float (Entry4.get())
+        English           = float (Entry5.get())
+        Arabic            = float (Entry6.get())
+        French            = float (Entry7.get())
+        Maths             = float (Entry8.get())
+        SVT               = float (Entry9.get())
+    except ValueError:
+        messagebox.showerror("Input Error", "Please fill all fields with numbers only.")
 
-    Physics           = float (Entry1.get())
-    HistGeo           = float (Entry2.get())
-    ICT               = float (Entry3.get())
-    EduIslamique      = float (Entry4.get())
-    English           = float (Entry5.get())
-    Arabic            = float (Entry6.get())
-    French            = float (Entry7.get())
-    Maths             = float (Entry8.get())
-    SVT               = float (Entry9.get())
-
+        return
     AuthenticEducation = (Physics * 0) + (HistGeo * 3) + (ICT * 2) + (EduIslamique * 4) + (English * 2) + (Arabic * 4) + (French * 3) + (Maths * 2) + (SVT * 2)
 
     ArtsAndHumanities  = (Physics * 0) + (HistGeo * 4) + (ICT * 2) + (EduIslamique * 0) + (English * 3) + (Arabic * 4) + (French * 4) + (Maths * 2) + (SVT * 2)
@@ -94,22 +129,37 @@ def Submit():
     Best = max(AuthenticEducation, ArtsAndHumanities, ScientificTrunk, TechnologicalStump)
 
     if (Best == AuthenticEducation):
-        BestLabel = ttk.Label(Frame, text = "The Best Orientation For You is: 'Authentic Education'", font = ("Arial", 13))
-        BestLabel.grid(row = 11, column = 0, padx = 15, pady = 15)
+        best_orientation = "Authentic Education"
 
     elif(Best == ArtsAndHumanities):
-        BestLabel = ttk.Label(Frame, text = "The Best Orientation For You is: 'Arts And Humanities'", font = ("Arial", 13))
-        BestLabel.grid(row = 11, column = 0, padx = 15, pady = 15)
+        best_orientation = "Arts And Humanities"
 
     elif(Best == ScientificTrunk):
-        BestLabel = ttk.Label(Frame, text = "The Best Orientation For You is: 'Scientific Trunk'", font = ("Arial", 13))
-        BestLabel.grid(row = 11, column = 0, padx = 15, pady = 15)
+        best_orientation = "Scientific Trunk"
 
     elif(Best == TechnologicalStump):
-        BestLabel = ttk.Label(Frame, text = "The Best Orientation For You is: 'Technological Stump'", font = ("Arial", 13))
-        BestLabel.grid(row = 11, column = 0, padx = 15, pady = 15)
+        best_orientation = "Technological Stump"
+
+    BestLabel = ttk.Label(Frame, text = f"The Best Orientation For You is: {best_orientation}", font = ("Arial", 13))
+    BestLabel.grid(row = 11, column = 0, padx = 15, pady = 15)
+
+    marks = {
+    "Physics": Physics,
+    "Hist.Geo": HistGeo,
+    "ICT": ICT,
+    "Edu.Islamique": EduIslamique,
+    "English": English,
+    "Arabic": Arabic,
+    "French": French,
+    "Maths": Maths,
+    "SVT": SVT
+    }
+
+    save_to_file(best_orientation, marks)
 #==========================================================================================================================================#
+# Creating the Submit button
 SubmitButton = tk.Button(Frame, text = "Submit", bg = "gray", command = Submit)
 SubmitButton.grid(row = 10, column = 0, columnspan = 2, pady = 10)
 #==========================================================================================================================================#
 root.mainloop()
+# This last line is important for that the app runs well and normally
